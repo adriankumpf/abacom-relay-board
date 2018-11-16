2e8facdf6ea13206d303faf2496be47a954fe13a
Adrian Kumpf <adrian.kumpf@posteo.de>
2018-11-16 11:49:40 +0100

    Use structopt to parse command line arguments

--- src/bin/arb.rs
+++ src/bin/arb.rs
@@ -1,84 +1,38 @@
-extern crate arb;
+use lazy_static::lazy_static;
+use structopt::StructOpt;
 
-#[macro_use]
-extern crate clap;
-extern crate libusb;
-
-use clap::{App, Arg};
-
-struct Args {
-    get_status: bool,
-    reset: bool,
-    relays: u8,
-    verify: bool,
-    port: Option<u8>,
+lazy_static! {
+    static ref RS: Vec<&'static str> = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 }
 
-fn parse_args() -> Args {
-    let matches = App::new("abacom-relay-board (arb)")
-        .author("Adrian K. <adrian.kumpf@posteo.de>")
-        .version(crate_version!())
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .help("Uses a custom port")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("disable-verification")
-                .short("d")
-                .long("disable-verification")
-                .requires("RELAYS")
-                .help("Disables the verifaction after activating relays"),
-        )
-        .arg(
-            Arg::with_name("get_status")
-                .short("s")
-                .long("status")
-                .help("Get relays status")
-                .conflicts_with("RELAYS"),
-        )
-        .arg(
-            Arg::with_name("reset")
-                .short("r")
-                .long("reset")
-                .help("Reset the relay board")
-                .conflicts_with("RELAYS"),
-        )
-        .arg(
-            Arg::with_name("RELAYS")
-                .help("Sets the relays to activate")
-                .default_value("0")
-                .possible_values(&["0", "1", "2", "3", "4", "5", "6", "7", "8"])
-                .multiple(true)
-                .index(1),
-        )
-        .get_matches();
+#[derive(StructOpt, Debug)]
+#[structopt(name = "abacom-relay-board (arb)")]
+struct Args {
+    /// Gets relays status
+    #[structopt(short, long, requires = "RELAYS")]
+    status: bool,
 
-    let port = value_t!(matches, "port", u8).ok();
-    let get_status = matches.is_present("get_status");
-    let reset = matches.is_present("reset");
-    let verify = !matches.is_present("disable-verification");
-    let relays = values_t!(matches, "RELAYS", u8)
-        .unwrap()
-        .iter()
-        .filter(|&&r| r != 0)
-        .fold(0, |acc, &r| acc | 1 << (r - 1));
+    /// Resets the relay board
+    #[structopt(short, long, conflicts_with = "RELAYS")]
+    reset: bool,
 
-    Args {
-        get_status,
-        reset,
-        relays,
-        verify,
-        port,
-    }
+    /// Disables the verifaction after activating relays
+    #[structopt(short, long = "disable-verifaction", requires = "RELAYS")]
+    disable_verification: bool,
+
+    /// Custom USB Port
+    #[structopt(short, long)]
+    port: Option<u8>,
+
+    /// The relays to activate
+    #[structopt(name = "RELAYS", default_value = "0", raw(possible_values = "&RS"))]
+    relays: Vec<u8>,
 }
 
 fn main() -> arb::Result {
-    let args = parse_args();
+    let args = Args::from_args();
 
-    if args.get_status {
+    if args.status {
         let result = arb::get_status(args.port)?;
 
         let active_relays: Vec<_> = (0..8)
@@ -93,10 +47,19 @@ fn main() -> arb::Result {
 
         println!("Active relays: {}", active_relays.join(" "));
 
-        Ok(())
-    } else if args.reset {
-        arb::reset(args.port)
-    } else {
-        arb::set_status(args.relays, args.verify, args.port)
+        return Ok(());
     }
+
+    if args.reset {
+        return arb::reset(args.port);
+    }
+
+    arb::set_status(
+        args.relays
+            .iter()
+            .filter(|&&r| r != 0)
+            .fold(0, |acc, &r| acc | 1 << (r - 1)),
+        args.disable_verification,
+        args.port,
+    )
 }
